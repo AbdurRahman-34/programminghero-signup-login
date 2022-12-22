1d5a979fbaff4357dc39c1a3a5b2de9fd9bd8f6b
AbdurRahman-34 <get.abdurahman@gmail.com>
2022-12-22 21:03:31 +0600

    last update

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
 import {Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState} from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import logo from '../../../../src/images/logo.svg'
+import { signOut } from 'firebase/auth';
+import auth from "../../../firebase.init";
 import './Header.css'
 
 const Header = () => {
+  const [user] = useAuthState(auth)
+  const handelLogOut = () => {
+    signOut(auth)
+  }
   return (
     <div className="main-header">
       <Navbar collapseOnSelect expand="lg"  variant="dark">
@@ -15,7 +22,13 @@ const Header = () => {
             <Nav className="ms-auto">
               <Nav.Link as={Link} to="/about">About Us</Nav.Link>
               <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              {
+                user
+                ?
+                <Nav.Link className="logout" onClick={handelLogOut}>SignOut</Nav.Link>
+                :
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              }
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/Components/Shared/Login/Login.js
+++ src/Components/Shared/Login/Login.js
@@ -1,13 +1,37 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import  auth from '../../../firebase.init'
 import './Login.css'
+import Loading from "../Loading/Loading";
 
 const Login = () => {
+
+  // All State 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [validated, setValidated] = useState(false);
+  const navigate = useNavigate()
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error1,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  const [sendPasswordResetEmail, sending, error2] = useSendPasswordResetEmail(auth);
 
 
+
+
+
+
+  // All Function 
   const handelEmailBlur = e => {
     setEmail(e.target.value)
   }
@@ -16,31 +40,77 @@ const Login = () => {
     setPassword(e.target.value)
   }
 
-  const handelLogin = e => {
-    
+  let errorElement;
+      if (error1 || error2) {
+      errorElement = <p className="text-danger">Error: {error1?.message || error2?.message}</p>
   }
+
+  if(loading || sending){
+    return <Loading/>
+  }
+
+
+  if(user){
+    navigate("/blog")
+  }
+  const handelLogin = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+    signInWithEmailAndPassword(email, password)
+  }
+
+
+  // Forget Password 
+  const forgetPassword = async() => {
+    if(email){
+      await sendPasswordResetEmail(email)
+      toast("send email")
+    }
+    else{
+        toast("Enter Your Email address")
+    }
+  }
+
+
+
+
 
   return (
     <div className="mt-5 form-container">
       <h1 className="text-center">Please Login</h1>
-      <Form onSubmit={handelLogin} className="form-main">
+      <Form noValidate validated={validated} onSubmit={handelLogin} className="form-main">
+
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control onBlur={handelEmailBlur} type="email" placeholder="Enter Email" />
+          <Form.Control onBlur={handelEmailBlur} type="email" placeholder="Enter Email" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
         </Form.Group>
 
 
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control onBlur={handelPasswordBlur} type="password" placeholder="Enter Password" />
+          <Form.Control onBlur={handelPasswordBlur} type="password" placeholder="Enter Password" required/>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Password
+          </Form.Control.Feedback>
         </Form.Group>
 
 
 
         <div className="d-flex justify-content-between">
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-               <Form.Check type="checkbox" label="Remember Me" />
+               <Form.Check type="checkbox" label="Remember Me" required 
+               feedback="You must agree before submitting."
+               feedbackType="invalid"/>
                 </Form.Group>
-             <p className="forget-password"><Link to="/signup">Forget Password</Link></p>
+             <p className="forget-password"><Link onClick={forgetPassword}>Forget Password</Link></p>
         </div>
 
 
@@ -48,6 +118,8 @@ const Login = () => {
 
         <p className="text-center mt-3 text-secondary">New user? <Link className="text-decoration-none" to="/signup">Register Here</Link></p>
       </Form>
+      {errorElement}
+      <ToastContainer/>
     </div>
   );
 };
